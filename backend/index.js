6c5539d367e82c30988551ed5f07b608fcd5974b
Barış Aytekin <barisaytekin5@gmail.com>
2023-10-25 16:08:45 +0300

    Changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,21 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connect = require("./config/db");
 const port = process.env.PORT || 5001;
-const cors = require("cors");
 
 connect();
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://barisaytekin.github.io/deployment/"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://barisaytekin.github.io/deployment/"
+  ); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use("/api", require("./routes/myRoutes"));
 app.listen(port, () => console.log(`Server running on port ${port}`));
