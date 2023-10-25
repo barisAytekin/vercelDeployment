1fa2314539ed51cd8510da5be6536043a0698288
Barış Aytekin <barisaytekin5@gmail.com>
2023-10-25 15:41:01 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connect = require("./config/db");
 const port = process.env.PORT || 5001;
+const cors = require("cors");
 
 connect();
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://barisaytekin.github.io/deployment/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use("/api", require("./routes/myRoutes"));
 app.listen(port, () => console.log(`Server running on port ${port}`));
